2091b96e70b8b2a97ae7eece1ef962b8c5c2cf96
Mateusz Dudek <mateuszd@biztech.pl>
2023-12-19 12:40:29 +0100

    stylowanie okna

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { Box, Paper } from '@mui/material';
-import './App.css';
-import Panel from './Panel';
+import { Box, Paper } from "@mui/material";
+import "./App.css";
+import Panel from "./Panel";
 
 function App() {
   return (
-    <Box sx={{
-      background: "#5f5",
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
+    <Box
+      sx={{
+        background: "#5f5",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Paper
         elevation={10}
         sx={{
           width: "1000px",
-          height: "680px",
+          height: "632px",
           display: "flex",
-        }}>
+        }}
+      >
         <Panel />
       </Paper>
     </Box>
--- src/Panel/index.tsx
+++ src/Panel/index.tsx
@@ -41,22 +41,40 @@ function Panel() {
       <Box sx={{ width: "80%" }}>
         <Box
           sx={{
-            width: "660px",
-            height: "660px",
+            width: "390px",
+            height: "612px",
             background: "#A99",
             display: "grid",
-            gridTemplate: "repeat(20, 40px) / repeat(10, 40px);",
-            rowGap: "2px",
-            columnGap: "2px",
+            gridTemplate: "repeat(20, 36px) / repeat(10, 36px);",
+            rowGap: "1px",
+            columnGap: "1px",
             paddingTop: "20px",
             paddingLeft: "20px",
           }}
         >
           {currentMatrix.flat(Infinity).map((currentValue) => {
             if (currentValue == 1) {
-              return <Box sx={{ background: "#b00" }}></Box>;
+              return (
+                <Box
+                  sx={{
+                    background: "#f00",
+                    webkitBoxShadow: "inset 0px 0px 20px 1px rgba(66, 68, 90, 1)",
+                    mozBoxShadow: "inset 0px 0px 20px 1px rgba(66, 68, 90, 1)",
+                    boxShadow: "inset 0px 0px 5px 1px rgba(66, 68, 90, 1)",
+                  }}
+                ></Box>
+              );
             } else {
-              return <Box sx={{ background: "#0b0" }}></Box>;
+              return (
+                <Box
+                  sx={{
+                    background: "#0b0",
+                    webkitBoxShadow: "inset 0px 0px 20px 1px rgba(66, 68, 90, 1)",
+                    mozBoxShadow: "inset 0px 0px 20px 1px rgba(66, 68, 90, 1)",
+                    boxShadow: "0px 0px 4px 1px rgba(66, 68, 90, 1)",
+                  }}
+                ></Box>
+              );
             }
           })}
         </Box>
